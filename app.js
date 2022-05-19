7124ed5b524d49b6af46095b9450e0086cf7b2d8
Hasnainqari <100525567+Hasnainqari@users.noreply.github.com>
2022-05-19 11:57:05 -0700

    Assignment3

---
+++ app.js
@@ -0,0 +1,31 @@
+// Question No #06;
+// var userMarksheetHeading = "Subjects TotalMarks ObtainedMarks Percentage";
+// document.write("<h2>" + userMarksheetHeading + "</h2>");
+
+// var english = "Engli "
+// var marksTotal = 100;
+// var marksObtained =  54;
+// var percentageTotal = " 54%";
+// document.write("<h1>"+english+marksTotal + " " +marksObtained + " " +percentageTotal +  "<h1>");
+
+// var urdu = "Urdu ";
+// var marksTotal1 = 100 ;
+// var marksObtained1 =  54 ;
+// var percentageTotal1 = " 54%";
+// document.write("<h1>" + urdu+marksTotal1+ " " +marksObtained1+percentageTotal1 + " " + "<h1>");
+
+// var math = "Math ";
+// var marksTotal2 = 100;
+// var marksObtained2 =  48 ;
+// var percentageTotal2 = " 48%";
+// document.write("<h1>" + math+marksTotal2+ " " +marksObtained2 + " " +percentageTotal2 + "<h1>");
+
+// var totalResult = "Total ";
+// document.write(totalResult+(marksTotal+marksTotal1+marksTotal2) +"  " );
+// document.write( marksObtained+marksObtained1+marksObtained2 + " ");
+
+// var totalMarksUser = "300";
+// var obtainedMarksUser ="156";
+// document.write(obtainedMarksUser/totalMarksUser*100);
+
+// Chapter NO #06 Complete;
